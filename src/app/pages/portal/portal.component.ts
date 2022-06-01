dd8d42214ba9aeb170c58ba31efdcb1982549d87
MAXIME LAMER <maxime.lamer@msp.gouv.qc.ca>
2022-06-01 08:59:40 -0400

    created branch simpleFilters

--- src/app/pages/portal/portal.component.ts
+++ src/app/pages/portal/portal.component.ts
@@ -113,11 +113,13 @@ import olFormatGeoJSON from 'ol/format/GeoJSON';
   ]
 })
 export class PortalComponent implements OnInit, OnDestroy {
+  public simpleFiltersValue$: BehaviorSubject<object> = new BehaviorSubject(undefined);
   public toastPanelOffsetX$: BehaviorSubject<string> = new BehaviorSubject(undefined);
   public sidenavOpened$: BehaviorSubject<boolean> = new BehaviorSubject(false);
   public clickedEntities$: BehaviorSubject<Feature[]> = new BehaviorSubject(undefined);
   public minSearchTermLength = 2;
   public hasExpansionPanel: boolean = false;
+  public showSimpleFilters: boolean = false;
   public showSimpleFeatureList: boolean = false;
   public hasGeolocateButton: boolean = true;
   public showMenuButton: boolean = true;
@@ -324,6 +326,7 @@ export class PortalComponent implements OnInit, OnDestroy {
     private directionState: DirectionState
   ) {
     this.hasExpansionPanel = this.configService.getConfig('hasExpansionPanel');
+    this.showSimpleFilters = this.configService.getConfig('simpleFilters') === undefined ? false : true;
     this.showSimpleFeatureList = this.configService.getConfig('simpleFeatureList') === undefined ? false : true;
     this.hasHomeExtentButton =
       this.configService.getConfig('homeExtentButton') === undefined ? false : true;
@@ -1489,5 +1492,9 @@ export class PortalComponent implements OnInit, OnDestroy {
         });
       }
     }
+  }
+
+  onFilterSelection(event: object) {
+    this.simpleFiltersValue$.next(event);
   }
 }
